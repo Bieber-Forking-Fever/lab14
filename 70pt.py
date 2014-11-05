67c9794404cba26cf2f7af22699ed13631105164
Bieber-Forking-Fever <asfiohqwtgpowh@gmail.com>
2014-11-05 15:03:55 -0600

    Done 70 pt

--- 70pt.py
+++ 70pt.py
@@ -31,72 +31,26 @@ class MyApp:
        	    self.up.configure(text="up", background= "green")
        	    self.up.grid(row=0,column=1)
 
-       	    self.left = Button(self.myContainer1)
-       	    self.left.configure(text="left", background= "green")
-       	    self.left.grid(row=1,column=0)
-
-       	    self.right = Button(self.myContainer1)
-       	    self.right.configure(text="Right", background= "green")
-       	    self.right.grid(row=1,column=2)
-
-       	    self.down = Button(self.myContainer1)
-       	    self.down.configure(text="Down", background= "green")
-       	    self.down.grid(row=3,column=1)
-
-
        	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
-            self.down.bind("<Button-1>", self.downClicked)
-            self.left.bind("<Button-1>", self.leftClicked)
-            self.right.bind("<Button-1>", self.rightClicked)
+
        	    
        	    # No need to edit this - just includes the drawpad into our frame
-       	    drawpad.pack(side=BOTTOM)
+       	    drawpad.pack()
 	
-	def animate(self):
-	    global drawpad
-	    global player
+
 	    # Remember to include your "enemies" with "global"
 	    	
 		
 	def upClicked(self, event):   
 	   global circle
 	   global player
+
+	   drawpad.move(player,0,-10)
+
            x1, y1, x2, y2 = drawpad.coords(player)
-           if  x1 > 199 and x2 < 281 and y1 <19 and y2 > 81:
-                drawpad.itemconfig(target,fill="red")
-	   drawpad.move(player,0,-20)
-	def downClicked(self, event):   
-	   global circle
-	   global player    
-           x1, y1, x2, y2 = drawpad.coords(player)
-	   if  x1 > 199 and x2 < 281 and y1 <19 and y2 > 81:
-                drawpad.itemconfig(target,fill="red")
-	   drawpad.move(player,0,20)
-	def leftClicked(self, event):   
-	   global circle
-	   global player
-           x1, y1, x2, y2 = drawpad.coords(player)
-           if  x1 > 199 and x2 < 281 and y1 <19 and y2 > 81:
-                drawpad.itemconfig(target,fill="red")
-	   drawpad.move(player,-20,0)
-	def rightClicked(self, event):   
-	   global circle
-	   global player
-           x1, y1, x2, y2 = drawpad.coords(player)
-           if  x1 > 199 and x2 < 281 and y1 <19 and y2 > 81:
-                drawpad.itemconfig(target,fill="red")
-	   drawpad.move(player,20,0)
-
-x1, y1, x2, y2 = drawpad.coords(player)
-
- 
-             
-
-
-        
-
-	
+           if (targetx1 < x1 and targetx2 > x2) and (targety1 < y1 and targety2 > y2):
+                drawpad.itemconfig(target, fill = "Red")
 		
 myapp = MyApp(root)
 
